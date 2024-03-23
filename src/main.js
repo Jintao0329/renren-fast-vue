578ac25bcd0bd9dc969cd98b58908a3a05b07228
Jintao0329 <jintaofu0329@gmail.com>
2024-03-23 22:42:28 +0100

    category

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import '@/assets/scss/index.scss'
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
